2c30ee8c8ff911a3f2c017261409292e0e3a0795
lsm1022 <luis.morel@unh.edu>
2025-12-31 18:52:26 -0500

    reworked the image connected lines code to use svg path

--- home_scripts.js
+++ home_scripts.js
@@ -1,32 +1,66 @@
-// This function is designed to dynamically adjusts the lines that connects the 
-// images in the body; initially based on the position of an image with the ID "pic_id" 
-function updateLines() {
-    const combos = document.querySelectorAll(".line_combo");
+// This function is designed to dynamically adjusts 
+// the line that connects the images in the body 
+function createSVGLines() {
 
-    combos.forEach(combo => {
-        const line = combo.querySelector(".custom_divider");
-        const rightLine = combo.querySelector(".right_line");
-        const leftLine = combo.querySelector(".left_line");
+    const path = document.getElementById("pathForLineID");
+    const container = document.getElementById('svgContainerID');
 
-        const pic = document.getElementById("pic_id");
-        const picRect = pic ? pic.getBoundingClientRect() : { x: 0, width: 70 };
+    //return if ids aren't found... shouldn't happen
+    if (!path || !container) return;
 
-        const width = (((picRect.x + picRect.width / 2) - (window.innerWidth / 2)) * 2);
-        line.style.width = `${width}px`;
+    const containerRect = container.getBoundingClientRect();
+    const rect1 = document.getElementById("picId1").getBoundingClientRect();
+    const rect2 = document.getElementById("picId2").getBoundingClientRect();
+    const rect3 = document.getElementById("picId3").getBoundingClientRect();
+    const rect4 = document.getElementById("picId4").getBoundingClientRect();
 
-        const lineRect = line.getBoundingClientRect();
+    //center points relative to document
+    const x1 = (rect1.left  + rect1.width / 2) - containerRect.left;
+    const y1 = (rect1.top  + rect1.height / 2) - containerRect.top;
+    const x2 = (rect2.left + rect2.width / 2) - containerRect.left;
+    const y2 = (rect2.top + rect2.height / 2) - containerRect.top;
+    const x3 = (rect3.left + rect3.width / 2) - containerRect.left;
+    const y3 = (rect3.top + rect3.height / 2) - containerRect.top;
+    const x4 = (rect4.left + rect4.width / 2) - containerRect.left;
+    const y4 = (rect4.top + rect4.height / 2) - containerRect.top;
 
-        rightLine.style.left = `${line.getBoundingClientRect().x + (lineRect.width)}px`;
-        leftLine.style.left = `${line.getBoundingClientRect().x}px`;
+    const diff = (rect1.left - rect2.left);
 
-    });
+    //pictues are not vertically aligned
+    if(diff > 0 && diff > 1 || diff < 0 && diff < -1){
 
+        const verticalDist = (y2 - y1) * 1.25;
+                
+        const cp1y = y1 + verticalDist;
+        const cp2y = y2 - verticalDist;
+        const cp3y = y3 + verticalDist;
+        const cp4y = y4 - verticalDist;
+
+        const d = `M ${x1} ${y1}
+        C ${x1} ${cp1y}, ${x2} ${cp2y}, ${x2} ${y2}
+        C ${x2} ${cp2y + verticalDist*2}, ${x3} ${cp3y - verticalDist*2}, ${x3} ${y3}
+        C ${x3} ${cp3y}, ${x4} ${cp4y}, ${x4} ${y4}`;
+
+        path.setAttribute('d', d);
+
+        //Another option for line connecting images (more rigid style)
+        // const dd = `M ${x1} ${y1}
+        // L ${x1} ${y1 +(y2-y1)/2}, ${x2} ${y1 + (y2-y1)/2}, ${x2} ${y2},
+        //  ${x2} ${y2 + (y3-y2)/2}, ${x3} ${y2 + (y3-y2)/2}, ${x3} ${y3},
+        //  ${x3} ${y3 + (y4-y3)/2}, ${x4} ${y3 + (y4-y3)/2}, ${x4} ${y4}`;
+        // path.setAttribute('d', dd);
+
+    }
+    else{//mobile view - simple line straight down
+        const d = `M ${x1} ${y1} L ${x4} ${y4}`;
+        path.setAttribute('d', d);
+    }
+    
 }
 
-//initial calls to update lines
-window.addEventListener('resize', updateLines);
-window.addEventListener('DOMContentLoaded', updateLines);
-
+window.addEventListener('load', () => createSVGLines());
+window.addEventListener('resize',() => createSVGLines());
+    
 function eefunction() {
     const img = document.getElementById("img_id");
     img.addEventListener("click", function () {
